47bb1d82ed66f943aa73247416fb095af504e87b
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-06 22:13:26 +0600

    server code added

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
 const express = require('express')
-
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
-
-
-const port = 5000;
-
-
-const app = express() 
-app.use(cors()); 
-app.use(bodyParser.json());
-
-
-
 const MongoClient = require("mongodb").MongoClient;
+const  ObjectId  = require("mongodb").ObjectId;
+const cors = require('cors');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nimyg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const app = express(); 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
 client.connect((err) => {
-  const bookings = client.db(process.env.DB_NAME).collection("data");
+  const events = client.db(process.env.DB_NAME).collection("data");
   
 console.log("database connected");
   app.post('/addEvent', (req, res) =>{
 
     const newEvent = req.body;
-    bookings.insertOne(newEvent)
-    .then(result =>  {
-        res.send(result.insertedCount > 0);
-    })
+    events.insertOne(newEvent).then((result) => {
+     res.send(result.insertedCount>0)
+    });
 
   })
-// app.get('/bookings', (req, res) => {
-//     bookings.find({email: req.headers.authorization})
-//     .toArray((err, documents) => {
-//         res.send(documents)
-//     })
-// })
+app.get('/getEvent', (req, res) => {
+    events.find({email: req.headers.authorization})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+})
+  app.get("/getEvent/:email", (req, res) => {
+    events
+      .find({ email: req.params.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  })
 
+
+
+her
+ app.delete('/delete/:id', (req, res) => {
+    events.deleteOne({_id: ObjectId(req.params.id)})
+    .then( result => {
+      res.send(result.deletedCount > 0)
+    })
+
+    
+  })
 });
 
 app.get('/', (req, res) => {
